901b4d0485bb8801d515caf99795a7b057dbf485
pleroux <p.leroux@gmx.com>
2018-01-27 11:17:26 +0100

    first commit

---
+++ runner.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   runner.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pierre <pleroux@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/27 10:46:33 by pierre            #+#    #+#             */
+/*   Updated: 2018/01/27 11:15:32 by pierre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "tddc.h"
+
+int tests_run = 0;
+
+int square(int x)
+{
+	return (x * x);
+}
+
+int square_01() {
+	int x=5;
+	_assert(square(x) == 25);
+	return 0;
+}
+
+int all_tests() {
+	_verify(square_01);
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	int result = all_tests();
+	if (result == 0)
+		printf("PASSED\n");
+	printf("Tests run: %d\n", tests_run);
+
+	return result != 0;
+}
---
+++ tddc.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tddc.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pierre <pleroux@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/27 11:08:13 by pierre            #+#    #+#             */
+/*   Updated: 2018/01/27 11:10:29 by pierre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef __TDDC_H_
+# define __TDDC_H_
+# include <stdio.h>
+
+#define FAIL() printf("\nfailure in %s() line %d\n", __func__, __LINE__)
+#define _assert(test) do { if (!(test)) { FAIL(); return 1; } } while(0)
+#define _verify(test) do { int r=test(); tests_run++; if(r) return r; } while(0)
+
+#endif
